27a3fdc82b1a804e0e7d76bc6b1c8b11c15bfa34
Ashish-Rao-2373 <125799384+Ashish-Rao-2373@users.noreply.github.com>
2025-08-23 02:22:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ if GDRIVE_CONNECTED:
                 expense_category = st.selectbox(
                     "Expense Category",
                     [
-                        "❤️ Girlfriend", "⛽ Fuel & Bike Service", "📱 Recharge & Subscriptions", "☕ Chai & snacks",
+                        "❤️ Girlfriend", "⛽ Fuel & Bike Service", "📱 Recharge & Subscriptions", "☕ Chai & snacks", "⚽ Sports",
                         "🍔 Food & Dining", "🛒 Groceries", "🚗 Transportation", "💡 Utilities", 
                         "🏠 Housing", "🛍️ Shopping", "🎬 Entertainment", "💪 Health & Fitness", 
                         "💄 Personal Care", "🎓 Education", "🎁 Gifts & Donations", "✈️ Travel", 
